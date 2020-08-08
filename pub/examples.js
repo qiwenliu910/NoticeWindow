62d23dcc971a4c3764f22c66b1d85fb47ae24143
Qiwen Liu <qiwen.liu@mail.utoronto.ca>
2020-08-08 09:54:49 -0400

    implemented image / gif  window

--- pub/examples.js
+++ pub/examples.js
@@ -6,7 +6,20 @@ const inputWindow = {
     header: "this is input header",
     content: "this is input content"
 }
-const cg = new WindowGenerator()
-cg.makeInputWindow(inputWindow)
-cg.makeNoticeWindow(noticeWindow)
+const imageWindow = {
+    url: "https://fas.calendar.utoronto.ca/section/Computer-Science",
+    imgSrc: 'tenor.gif'}
+const newInputWindow = new WindowGenerator()
+const newNoticeWindow = new WindowGenerator()
+const newImageWindow = new WindowGenerator()
+
+newInputWindow.makeInputWindow(inputWindow)
+newNoticeWindow.makeNoticeWindow(noticeWindow)
+newImageWindow.makeImageWindow(imageWindow.url, imageWindow.imgSrc)
+
+newNoticeWindow.setNoticeWindowHeader("nihaoma")
+newNoticeWindow.setNoticeWindowContent("wozuijizhi")
+
+newInputWindow.setInputWindowHeader("wohenhao")
+newInputWindow.setInputWindowContent("wozuikeai")
 
--- pub/notify.js
+++ pub/notify.js
@@ -1,45 +1,71 @@
 function WindowGenerator() {
-  this.windows = []  
-	// this..
-	// this.. (any values you need for each 'instance' of this library)
+  this.defaultNoticeWindow = {
+    header: "this is the notice header",
+    content: "this is the notice content"
+  } 
+  this.defaultInputWindow = defaultInputWindow = {
+    header: "this is the input header",
+    content: "this is the input content"
+  }
+  this.defaultImageWindow = defaultInputWindow = {
+    header: "Congrats, click the image to proceed"
+  }
 }
 
 WindowGenerator.prototype = {
 
 	makeNoticeWindow: function(inputObject) {
-    const defaultNoticeWindow = {
-      header: "this is header",
-      content: "this is content"
-  }
-  const noticeWindow = Object.assign(defaultNoticeWindow, inputObject)
-    let window = '<div class="notice-popup-overlay"><div class="notice-popup-content"><h2>'.concat(noticeWindow.header,'</h2><p>',noticeWindow.content,'.</p><button id = "closeButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+   
+  const noticeWindow = Object.assign(this.defaultNoticeWindow, inputObject)
+    let window = '<div class="notice-popup-overlay"><div class="notice-popup-content"><h2>'.concat('<div class="noticeWindowHeader">',noticeWindow.header,'</div></h2><p><div class="noticeWindowContent">',noticeWindow.content,'.</div></p><button id = "closeNoticeButton" onclick="handleClick()" class="close">Close</button> </div></div>')
     $('#NoticeWindowLocation').html(window);
     const openButton = document.querySelector('#openNoticeButton');
     openButton.onclick = function(event) {  
         $(".notice-popup-overlay, .notice-popup-content").addClass("active"); 
     }
-    const closeButton = document.querySelector('#closeButton');
+    const closeButton = document.querySelector('#closeNoticeButton');
     closeButton.onclick = function(event) {
       $(".notice-popup-overlay, .notice-popup-content").removeClass("active");
     }
   },
-
+  setNoticeWindowHeader: function (inputString) {
+    $('.noticeWindowHeader').html(inputString);
+  },
+  setNoticeWindowContent: function (inputString) {
+    $('.noticeWindowContent').html(inputString);
+  },
   makeInputWindow: function (inputObject) {
-    const defaultInputWindow = {
-      header: "this is header",
-      content: "this is content"
-  }
-  const inputWindow = Object.assign(defaultInputWindow, inputObject)
    
-    let window = '<div class="input-popup-overlay"><div class="input-popup-content"><h2>'.concat(inputWindow.header,'</h2><p>',inputWindow.content,'</p><form><label for="fname">First name:</label><br><input type="text" id="fname" name="fname" value=""><br><label for="lname">Last name:</label><br><input type="text" id="lname" name="lname" value=""><br><br><input type="submit" value="Submit"></form><br><button id = "closeButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+  const inputWindow = Object.assign(this.defaultInputWindow, inputObject)
+   
+    let window = '<div class="input-popup-overlay"><div class="input-popup-content"><h2>'.concat('<div class="inputWindowHeader">',inputWindow.header,'</div></h2><p><div class="inputWindowContent">',inputWindow.content,'.</div></p><form><label for="fname">First name:</label><br><input type="text" id="fname" name="fname" value=""><br><label for="lname">Last name:</label><br><input type="text" id="lname" name="lname" value=""><br><br><input type="submit" value="Submit"></form><br><button id = "closeInputButton" onclick="handleClick()" class="close">Close</button> </div></div>')
     $('#InputWindowLocation').html(window);
     const openButton = document.querySelector('#openInputButton');
     openButton.onclick = function(event) {  
         $(".input-popup-overlay, .input-popup-content").addClass("active");     
     }
-    const closeButton = document.querySelector('#closeButton');
+    const closeButton = document.querySelector('#closeInputButton');
     closeButton.onclick = function(event) {
       $(".input-popup-overlay, .input-popup-content").removeClass("active");
+    }
+
+  },
+  setInputWindowHeader: function (inputString) {
+    $('.inputWindowHeader').html(inputString);
+  },
+  setInputWindowContent: function (inputString) {
+    $('.inputWindowContent').html(inputString);
+  },
+  makeImageWindow: function (url, imgSrc) {
+    const imageWindow = '<div class="image-popup-overlay"><div class="image-popup-content"><h2>'.concat('<div class="ImageWindowHeader">',this.defaultImageWindow.header,'</div></h2><a href =" ',url,'" target = "_blank"><img src = "',imgSrc,'"/></a><br><button id = "closeImageButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+    $('#ImageWindowLocation').html(imageWindow);
+    const openButton = document.querySelector('#openImageButton');
+    openButton.onclick = function(event) {  
+        $(".image-popup-overlay, .image-popup-content").addClass("active");     
+    }
+    const closeButton = document.querySelector('#closeImageButton');
+    closeButton.onclick = function(event) {
+      $(".image-popup-overlay, .image-popup-content").removeClass("active");
     }
 
   },
