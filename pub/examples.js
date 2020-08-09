6a7ed7756cda46e65448a215dc0a0e93fc89b6e0
Qiwen Liu <qiwen.liu@mail.utoronto.ca>
2020-08-09 16:26:04 -0400

    implemented timer for each window

--- pub/examples.js
+++ pub/examples.js
@@ -1,21 +1,27 @@
 const noticeWindow = {
+    time:5000,
     header: "this is notice header",
     content: "this is notice content"
 }
 const inputWindow = {
+    time:null,
     header: "this is input header",
     content: "this is input content"
 }
 const imageWindow = {
+    time:4000,
+    header: "congrats, click the image to proceed",
     url: "https://fas.calendar.utoronto.ca/section/Computer-Science",
-    imgSrc: 'tenor.gif'}
+    imgSrc: 'image/tenor.gif'}
 const newInputWindow = new WindowGenerator()
 const newNoticeWindow = new WindowGenerator()
 const newImageWindow = new WindowGenerator()
+const newRotateImageWindow = new WindowGenerator()
 
 newInputWindow.makeInputWindow(inputWindow)
 newNoticeWindow.makeNoticeWindow(noticeWindow)
-newImageWindow.makeImageWindow(imageWindow.url, imageWindow.imgSrc)
+newImageWindow.makeImageWindow(imageWindow)
+newRotateImageWindow.makeRotateImageWindow(imageWindow.url, "image/5070-512.png")
 
 newNoticeWindow.setNoticeWindowHeader("nihaoma")
 newNoticeWindow.setNoticeWindowContent("wozuijizhi")
--- pub/notify.js
+++ pub/notify.js
@@ -1,14 +1,17 @@
 function WindowGenerator() {
   this.defaultNoticeWindow = {
+    time: null,
     header: "this is the notice header",
     content: "this is the notice content"
   } 
-  this.defaultInputWindow = defaultInputWindow = {
+  this.defaultInputWindow = {
+    time:null,
     header: "this is the input header",
     content: "this is the input content"
   }
-  this.defaultImageWindow = defaultInputWindow = {
-    header: "Congrats, click the image to proceed"
+  this.defaultImageWindow = {
+    time:null,
+    header: "this is the image header"
   }
 }
 
@@ -17,11 +20,16 @@ WindowGenerator.prototype = {
 	makeNoticeWindow: function(inputObject) {
    
   const noticeWindow = Object.assign(this.defaultNoticeWindow, inputObject)
-    let window = '<div class="notice-popup-overlay"><div class="notice-popup-content"><h2>'.concat('<div class="noticeWindowHeader">',noticeWindow.header,'</div></h2><p><div class="noticeWindowContent">',noticeWindow.content,'.</div></p><button id = "closeNoticeButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+    let window = '<button id = "openNoticeButton" onclick="handleClick()" class="open">Open Notice Window</button><div class="notice-popup-overlay"><div class="notice-popup-content"><h2>'.concat('<div class="noticeWindowHeader">',noticeWindow.header,'</div></h2><p><div class="noticeWindowContent">',noticeWindow.content,'.</div></p><button id = "closeNoticeButton" onclick="handleClick()" class="close">Close</button> </div></div>')
     $('#NoticeWindowLocation').html(window);
     const openButton = document.querySelector('#openNoticeButton');
     openButton.onclick = function(event) {  
         $(".notice-popup-overlay, .notice-popup-content").addClass("active"); 
+        if (noticeWindow.time !== null) {
+          setTimeout(function(){
+            $(".notice-popup-overlay, .notice-popup-content").removeClass("active");
+           }, noticeWindow.time);   
+        }
     }
     const closeButton = document.querySelector('#closeNoticeButton');
     closeButton.onclick = function(event) {
@@ -38,11 +46,16 @@ WindowGenerator.prototype = {
    
   const inputWindow = Object.assign(this.defaultInputWindow, inputObject)
    
-    let window = '<div class="input-popup-overlay"><div class="input-popup-content"><h2>'.concat('<div class="inputWindowHeader">',inputWindow.header,'</div></h2><p><div class="inputWindowContent">',inputWindow.content,'.</div></p><form><label for="fname">First name:</label><br><input type="text" id="fname" name="fname" value=""><br><label for="lname">Last name:</label><br><input type="text" id="lname" name="lname" value=""><br><br><input type="submit" value="Submit"></form><br><button id = "closeInputButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+    let window = '<button id = "openInputButton" onclick="handleClick()" class="open">Open Input Window</button><div class="input-popup-overlay"><div class="input-popup-content"><h2>'.concat('<div class="inputWindowHeader">',inputWindow.header,'</div></h2><p><div class="inputWindowContent">',inputWindow.content,'.</div></p><form><label for="fname">First name:</label><br><input type="text" id="fname" name="fname" value=""><br><label for="lname">Last name:</label><br><input type="text" id="lname" name="lname" value=""><br><br><input type="submit" value="Submit"></form><br><button id = "closeInputButton" onclick="handleClick()" class="close">Close</button> </div></div>')
     $('#InputWindowLocation').html(window);
     const openButton = document.querySelector('#openInputButton');
     openButton.onclick = function(event) {  
-        $(".input-popup-overlay, .input-popup-content").addClass("active");     
+        $(".input-popup-overlay, .input-popup-content").addClass("active"); 
+        if (inputWindow.time !== null) {
+          setTimeout(function(){
+            $(".input-popup-overlay, .input-popup-content").removeClass("active");
+           }, inputWindow.time);   
+        }    
     }
     const closeButton = document.querySelector('#closeInputButton');
     closeButton.onclick = function(event) {
@@ -56,16 +69,43 @@ WindowGenerator.prototype = {
   setInputWindowContent: function (inputString) {
     $('.inputWindowContent').html(inputString);
   },
-  makeImageWindow: function (url, imgSrc) {
-    const imageWindow = '<div class="image-popup-overlay"><div class="image-popup-content"><h2>'.concat('<div class="ImageWindowHeader">',this.defaultImageWindow.header,'</div></h2><a href =" ',url,'" target = "_blank"><img src = "',imgSrc,'"/></a><br><button id = "closeImageButton" onclick="handleClick()" class="close">Close</button> </div></div>')
-    $('#ImageWindowLocation').html(imageWindow);
+  makeImageWindow: function (inputObject) {
+    const ImageWindow = Object.assign(this.defaultImageWindow, inputObject)
+    const window = '<button id = "openImageButton" onclick="handleClick()" class="open">Open Image Window</button><div class="image-popup-overlay"><div class="image-popup-content"><h2>'.concat('<div class="ImageWindowHeader">',ImageWindow.header,'</div></h2><a href =" ',ImageWindow.url,'" target = "_blank"><img src = "',ImageWindow.imgSrc,'"/></a><br><button id = "closeImageButton" onclick="handleClick()" class="close">Close</button> </div></div>')
+    $('#ImageWindowLocation').html(window);
     const openButton = document.querySelector('#openImageButton');
     openButton.onclick = function(event) {  
-        $(".image-popup-overlay, .image-popup-content").addClass("active");     
+        $(".image-popup-overlay, .image-popup-content").addClass("active");  
+        if (ImageWindow.time !== null) {
+          setTimeout(function(){
+            $(".image-popup-overlay, .image-popup-content").removeClass("active");
+           }, ImageWindow.time);   
+        }    
     }
     const closeButton = document.querySelector('#closeImageButton');
     closeButton.onclick = function(event) {
       $(".image-popup-overlay, .image-popup-content").removeClass("active");
+    }
+
+  },
+  makeRotateImageWindow: function (url, imgSrc) {
+    const imageWindow = '<button id = "openRotateImageButton" onclick="handleClick()" class="open">Open Rotate Image Window</button><div class="rotate-image-popup-overlay"><div class="rotate-image-popup-content"><h2>'.concat('<div class="RotateImageWindowHeader">',this.defaultImageWindow.header,'</div></h2><div class = "imagePosition"><a href =" ',url,'" target = "_blank"><img src = "',imgSrc,'"/></a></div><br><button id = "leftImageButton" onclick="handleClick()" class="left">left</button><button id = "closeRotateImageButton" onclick="handleClick()" class="close">Close</button><button id = "rightImageButton" onclick="handleClick()" class="right">right</button> </div></div>')
+    $('#RotateImageWindowLocation').html(imageWindow);
+    const openButton = document.querySelector('#openRotateImageButton');
+    openButton.onclick = function(event) {  
+        $(".rotate-image-popup-overlay, .rotate-image-popup-content").addClass("active");     
+    }
+    const closeButton = document.querySelector('#closeRotateImageButton');
+    closeButton.onclick = function(event) {
+      $(".rotate-image-popup-overlay, .rotate-image-popup-content").removeClass("active");
+    }
+    const leftButton = document.querySelector('#leftImageButton');
+    leftButton.onclick = function(event) {
+      $('.imagePosition img').css('transform', "rotate(90deg)")
+    }
+    const rightButton = document.querySelector('#rightImageButton');
+    rightButton.onclick = function(event) {
+      $('.imagePosition img').css('transform', "rotate(-90deg)")
     }
 
   },
